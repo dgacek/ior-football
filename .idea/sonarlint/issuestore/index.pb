b505e07bdb585ca22ad395867214e3ec56824fc9
Dawid Gacek <d.gacek@wasko.pl>
2022-12-10 20:31:47 +0100

    hibernate conf fix

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -10,3 +10,13 @@
 settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
 X
 (gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+w
+G.idea/sonarlint/issuestore/5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9,f\7\f7819ea138c33fec28e7e536cb2de34db38fc121
+w
+G.idea/sonarlint/issuestore/2/a/2a45a911a8f1836b0b6c5b758962572012d8f8c3,9\a\9aed88f31eac276604c6355c1adcc0bce1f80fd6
+w
+G.idea/sonarlint/issuestore/a/5/a5cc2925ca8258af241be7e5b0381edf30266302,e\d\ed67eb178d523a4b3f117dcbe0eda5196367ceec
+w
+G.idea/sonarlint/issuestore/f/b/fbe448ebfc3eb2d4e308f6b8b043666f5b57235e,5\2\52114fdaa426e9b83a3c5df4e11c4f0ebbcd814b
+w
+G.idea/sonarlint/issuestore/0/5/05efc8b1657769a27696d478ded1e95f38737233,3\c\3c8c56407c35e5f5ebbd6c7250a79af8128a13a6
--- src/main/java/ior/project/Main.java
+++ src/main/java/ior/project/Main.java
@@ -1,7 +1,19 @@
 package ior.project;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+            StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+            sb.applySettings(cfg.getProperties());
+            StandardServiceRegistry standardServiceRegistry = sb.build();
+            SessionFactory sessionFactory = cfg.buildSessionFactory(standardServiceRegistry);
+            Session session = sessionFactory.getCurrentSession();
     }
 }
