ddf91bc8bd7a116842a77c0a8f8354794dbaa8e2
Dawid Gacek <d.gacek@wasko.pl>
2022-12-10 21:18:24 +0100

    prepare main 2

--- src/main/java/ior/project/Main.java
+++ src/main/java/ior/project/Main.java
@@ -1,5 +1,6 @@
 package ior.project;
 
+import ior.project.model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -8,15 +9,27 @@ import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-        StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
-        sb.applySettings(cfg.getProperties());
-        StandardServiceRegistry standardServiceRegistry = sb.build();
-        SessionFactory sessionFactory = cfg.buildSessionFactory(standardServiceRegistry);
-        Session session = sessionFactory.getCurrentSession();
+        try {
+            Configuration cfg = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Coach.class)
+                    .addAnnotatedClass(Match.class)
+                    .addAnnotatedClass(Person.class)
+                    .addAnnotatedClass(Player.class)
+                    .addAnnotatedClass(Position.class)
+                    .addAnnotatedClass(Stadium.class)
+                    .addAnnotatedClass(Team.class);
+            StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+            sb.applySettings(cfg.getProperties());
+            StandardServiceRegistry standardServiceRegistry = sb.build();
+            SessionFactory sessionFactory = cfg.buildSessionFactory(standardServiceRegistry);
+            Session session = sessionFactory.getCurrentSession();
 
-        //TUTAJ WSTAW BAJER MARCIN
+            //TUTAJ WSTAW BAJER MARCIN
 
-        session.close();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
